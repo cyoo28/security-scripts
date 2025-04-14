00199fc222f0057f4a5191cd7ee926d8bbe3037b
cyoo28 <charlieyoo00@gmail.com>
2025-04-14 17:01:09 -0400

    Minor variable name change again and fixed error handling

--- role_trust_check.py
+++ role_trust_check.py
@@ -2,7 +2,7 @@
 import sys
 import boto3
 
-def getRoleInfo(iam, awsPrincipal, role, myAccount, orgAccounts, extRoles, intRoles, delRoles):
+def getRoleInfo(iam, awsPrincipal, role, myAccount, orgAccounts, extRoles, intRoles, unknownRoles):
     # Get the name of the role, its creation date, and the date that it was last used
     roleName = role["RoleName"]
     createDate = role["CreateDate"].strftime('%Y-%m-%d %H:%M:%S %Z')
@@ -12,12 +12,13 @@ def getRoleInfo(iam, awsPrincipal, role, myAccount, orgAccounts, extRoles, intRo
     except:
         lastUseDate = "No activity"
     # Add the value to the referenced dictionary
-    if awsPrincipal.split(":")[4] not in orgAccounts:
-        extRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
-    elif awsPrincipal.split(":")[4] != myAccount:
-        intRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
+    if ":" in awsPrincipal:
+        if awsPrincipal.split(":")[4] not in orgAccounts:
+            extRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
+        elif awsPrincipal.split(":")[4] != myAccount:
+            intRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
     else:
-        delRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
+        unknownRoles[roleName] = {"Creation Date":createDate,"Last Used":lastUseDate}
 
 def main(iam, sts, org):
     # Set up the paginators
@@ -37,7 +38,7 @@ def main(iam, sts, org):
     # Set up the dictionaries that contain the roles with cross-account access
     extRoles = {} # for roles with trust to accounts external to the organization
     intRoles = {} # for roles with trust to accounts internal to the organization
-    delRoles = {} # for roles with trust to an entity that no longer exists
+    unknownRoles = {} # for roles with trust to an entity that may no longer exist
 
     # Iterate through the paginator
     iamIterator = iamPaginator.paginate()
@@ -49,7 +50,7 @@ def main(iam, sts, org):
             # Iterate through each assume role policy statement (a role could have more than one)
             for statement in role["AssumeRolePolicyDocument"]["Statement"]:
                 # Stop iterating through the statements if it has already been confirmed that this role has cross-account access
-                if role["RoleName"] in extRoles or role["RoleName"] in intRoles or role["RoleName"] in delRoles:
+                if role["RoleName"] in extRoles or role["RoleName"] in intRoles or role["RoleName"] in unknownRoles:
                     break
                 else:
                     # Check that the statement is for an AWS principal
@@ -59,7 +60,7 @@ def main(iam, sts, org):
                         if isinstance(awsPrincipals, str):
                             awsPrincipals = [awsPrincipals]
                         for awsPrincipal in awsPrincipals:
-                            getRoleInfo(iam, awsPrincipal, role, myAccount, orgAccounts, extRoles, intRoles, delRoles)
+                            getRoleInfo(iam, awsPrincipal, role, myAccount, orgAccounts, extRoles, intRoles, unknownRoles)
     # Write the results to the file
     with open(fileName, "a") as file:
         if not extRoles:
@@ -72,9 +73,9 @@ def main(iam, sts, org):
         else:
             for key, value in intRoles.items():
                 file.write("{},{},{},{}\n".format("Internal", key, value["Creation Date"], value["Last Used"]))
-        if delRoles:
+        if unknownRoles:
             for key, value in intRoles.items():
-                file.write("{},{},{},{}\n".format("Deleted", key, value["Creation Date"], value["Last Used"]))
+                file.write("{},{},{},{}\n".format("Unknown", key, value["Creation Date"], value["Last Used"]))
 
 if __name__ == "__main__":
     # verify that all arguments are used
